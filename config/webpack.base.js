655889afa2c9287925c0fda0f3e02f234daa64fe
mingjiu <boss_think@163.com>
2025-02-24 20:38:56 +0800

    feat: add support postcss

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const autoprefixer = require('autoprefixer');
 const { srcPath, distPath } = require('./paths')
 
 module.exports = {
@@ -14,9 +15,14 @@ module.exports = {
         include: srcPath,
         exclude: /node_modules/
       },
+      // 针对 css 文件处理，增加 'postcss-loader'，loader处理顺序从后向前
       {
         test: /\.css$/,
-        loader: ['style-loader', 'css-loader']
+        loader: [
+          'style-loader', 
+          'css-loader',
+          'postcss-loader'
+        ]
       }
     ]
   },
