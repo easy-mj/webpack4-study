1aa01171c1a4127cb9b41239d321de086b240184
mingjiu <boss_think@163.com>
2025-02-25 19:50:37 +0800

    perf: add production mode

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,6 +6,15 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { srcPath, distPath } = require('./paths')
 
 module.exports = smart(webpackBaseConfig, {
+  /**
+   * 使用 production 模式
+   * 1. 自动开启代码压缩，如果觉得uglifyJS压缩慢，可以借助ParallelUglifyPlugin来开启多进程压缩
+   * 2. Vue React等会自动删掉调试代码（比如开发环境的warning等）
+   * 3. 启动 Tree-Shaking，作用就是会删除没有用到的代码
+   *   3.1 必须使用ES6 Module才能生效，因为ES6 Module是静态引用，编译时引入
+   *   3.2 CommonJS是动态引入，是执行时引入
+   *   3.3 只有 ES Module 才能实现静态分析，从而实现 Tree-Shaking
+   */
   mode: 'production',
   output: {
     filename: '[name].[contentHash:8].js',
