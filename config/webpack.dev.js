36fab11fa8c933e779e775d45802cc1fa3e62929
mingjiu <boss_think@163.com>
2025-02-25 15:30:23 +0800

    perf: add watch

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -21,6 +21,16 @@ module.exports = smart(webpackBaseConfig, {
       APP_ENV: JSON.stringify('developement')
     })
   ],
+  // 开启监听，默认是 false，注意开启监听后，webpack-dev-server会自动开启刷新浏览器（webpack-dev-server已经默认开启）
+  watch: true,
+  // 监听配置
+  watchOptions: {
+    ignored: /node_modules/,
+    // 监听到变化发生后会等300ms再去执行动作，防止文件更新太快导致重新编译频率太高
+    aggregateTimeout: 300, // 默认为300ms
+    // 判断文件是否发生变化时通过不停的去询问系统指定文件有没有变化实现的
+    poll: 1000 * 5 // 默认每隔1000ms询问一次
+  },
   devServer: {
     port: 8080,
     progress: true,  // 显示打包的进度条
