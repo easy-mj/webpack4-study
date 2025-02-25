5ff3478b277051ba162b4bec244e1b2c54494006
mingjiu <boss_think@163.com>
2025-02-25 09:22:54 +0800

    perf: add babel loader cache

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -10,9 +10,10 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        loader: ['babel-loader'],
-        include: srcPath,
-        exclude: /node_modules/
+        // 开启缓存（第一次编译后会缓存，如果第二次没有改动的部分会使用缓存而不是重新编译）
+        loader: ['babel-loader?cacheDirectory'],
+        include: srcPath, // 明确查找范围（include 和 exclude选择其一即可）
+        exclude: /node_modules/ // 排除查找范围
       },
       {
         test: /\.css$/,
